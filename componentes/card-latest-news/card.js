be051e836f45bc919a89be10c5f7633b3f1a7484
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-09-16 18:53:53 -0300

    feat: adiciona um componente do card de últimas news

---
+++ componentes/card-latest-news/card.js
@@ -0,0 +1,80 @@
+async function getDataLatestNews() {
+  const reponse = await fetch("data/latestNews.json");
+  const data = await reponse.json();
+  return data;
+}
+const dataLatestNews = getDataLatestNews();
+
+const htmlCard = {
+  getElement(element) {
+    return document.querySelector(element);
+  },
+  getElements(element) {
+    return document.querySelectorAll(element);
+  },
+  createElementAndClass(element, ...classes) {
+    if (element) {
+      const genericElement = document.createElement(element);
+      if (classes.length > 0) {
+        genericElement.classList.add(...classes);
+      }
+      return genericElement;
+    }
+    return;
+  },
+};
+
+const createPost = {
+  container(data) {
+    const containerLatestNews = htmlCard.getElement(".content-latest-news");
+    const postContainer = htmlCard.createElementAndClass(
+      "div",
+      "post",
+      `post-data-${data.id}`
+    );
+
+    postContainer.innerHTML = `
+      <div class="container-img-latest">
+        <a href="#">
+          <img
+            class="img-post"
+            src="${data.imageUrl}"
+            alt="${data.title}"
+          />
+        </a>
+      </div>
+
+      <div class="content-post">
+        <span class="category-post category-post-ln">${data.category}</span>
+        <a href="#">
+          <span class="title-post">${data.title}</span>
+        </a>
+        <span class="preview-description">${data.content}</span>
+        <div class="post-details">
+          <div class="author-details">
+            <i class="bi bi-person-square"></i>
+            <span>${data.author.name}</span>
+          </div>
+          <div class="statistic-post">
+            <span class="post-date">
+              <span class="value">${data.createdAt}</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    
+    `;
+
+    containerLatestNews.appendChild(postContainer);
+  },
+};
+
+async function init() {
+  const dataLatestNews = await getDataLatestNews();
+
+  dataLatestNews.map((data) => {
+    createPost.container(data);
+  });
+}
+
+init();
